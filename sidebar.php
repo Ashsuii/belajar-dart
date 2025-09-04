e310de3ea6270a573509679975bfd8820647aed3
Ashsuii <ariaprianto.lp3i.krw24@gmail.com>
2025-09-04 11:25:29 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,6 @@
+    <div class="sidebar">
+        <h4>Test</h4>
+        <hr>
+        <a href="tampilData.php"><i class="fas fa-user"></i>Data Pendaftar</a>
+        <a href="#"><i class="fas fa-user"></i>Lorem Ipsum</a>
+    </div>
